29708c3a7536d196a41c1432c3a005aea675f814
朱嘉豪 <zhujiahao@MacBook-Pro.local>
2020-07-31 17:31:46 +0800

    update

--- assetscan/toolsLib/icmp.py
+++ assetscan/toolsLib/icmp.py
@@ -1,6 +1,25 @@
+import os
+import struct
+import array
+import socket
+
+
 class icmp():
-    def __init__(self, allIp):
+    def __init__(self, allIp, timeout=3):
+        self.timeout = timeout
         self.allIp = allIp
+        self.__data = struct.pack('h', 1)
+        self.__id = os.getpid()
+        if self.__id >= 65535: self.__id = 65534
+
+    def __checkSum(self, packet):
+        shorts = array.array('H', packet)
+        sum = 0
+        for short in shorts:
+            sum += short & 0xffff
+        while (sum >> 16) > 0:
+            sum = (sum & 0xffff) + (sum >> 16)
+        return (~sum) & 0xffff
 
     @property
     def __icmpPacket(self):
@@ -9,8 +28,9 @@ class icmp():
 
     @property
     def __sock(self):
-        pass
-        return
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.settimeout(self.timeout)
+        return sock
 
     def run(self):
         return
